c4759fcebd57c0ddaf97cc81d0d2cf350666598d
Annie Fryman <afryman.92@gmail.com>
2015-12-22 13:07:45 -0800

    take 2: now the escape route is faster, more limber

--- 122115 first date.js
+++ 122115 first date.js
@@ -1,21 +1,23 @@
 
 var test1 = prompt("Dogs or Cats?");
-var test2 = prompt("Stones or Beatles?");
-var test3 = prompt("2Pac or Biggie?");
-
 var dateNumber = 1;
+var escape = "This guy is a nut. Run away!";
 
 if (test1 == "Dogs"){
         console.log("He loves dogs. Case closed.");
         dateNumber += 1;
-    } else if (test1 == "Cats"){
+    } else if (test1 != "Dogs"){
+        var test2 = prompt("Stones or Beatles?");
+        var test3 = prompt("2Pac or Biggie?");
         if (test2 == "Beatles" && test3 == "Biggie"){
-            console.log("This guy is a nut. Run away!");
+            console.log(escape);
         } else if (test2 == "Stones" || test3 == "2Pac") {
             console.log("Be skeptical. Be very, very skeptical."); 
             var chitecture = prompt("Do you like Victorian houses?");
             if (chitecture != "yes"){
-            dateNumber += 1;
-        }
+                dateNumber += 1;
+            } else {
+                console.log(escape);
+                }
+            }
     }
-}
